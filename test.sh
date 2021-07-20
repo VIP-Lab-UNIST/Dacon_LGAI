6481244178f441a2caee686805aaede83f4a7fbb
root <khko@unist.ac.kr>
2021-07-20 11:03:46 +0900

    autotest

--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
-CUDA_VISIBLE_DEVICES=0 python3 main.py test \
-                                --data-dir ../Datasets \
-                                --save-dir ./logs/train/DWGAN/202107151742/test \
-                                --resume ./logs/train/DWGAN/202107151742/checkpoint_059.tar
+for epoch in {25..20..1}
+    do
+        CUDA_VISIBLE_DEVICES=0 python3 main.py test \
+                                        --data-dir ../Datasets \
+                                        --save-dir ./logs/train/DWGAN_multiscale/202107191153/outputs \
+                                        --resume ./logs/train/DWGAN_multiscale/202107191153/checkpoint_0$epoch.tar
 
-                                # --save-dir ./logs/train/202107012237/stride_128 \
-                                # --save-dir ./logs/test/202107012237/checkpoint_025/stride_32 \
+                                        # --save-dir ./logs/train/202107012237/stride_128 \
+                                        # --save-dir ./logs/test/202107012237/checkpoint_025/stride_32 \
+    done
